d1f4d7585f1a52cd7510fd20169a9b9f1a25c3a3
hungkito <hungpvth2008007@fpt.edu.vn>
2021-10-06 21:35:53 +0700

    Add project files.

---
+++ ASM1/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM1.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class StudentMamager
+    {
+        public static List<Student> GetStudents()
+        {
+            var Students = new List<Student>();
+
+            Students.Add(new Student { Name = "StudentA", Image = "Assets/female-01.png", Age = 2002 });
+            Students.Add(new Student { Name = "StudentB", Image = "Assets/male-01.png", Age = 2002 });
+            Students.Add(new Student { Name = "StudentC", Image = "Assets/female-03.png", Age = 2002 });
+            return Students;
+        }
+    }
+}
